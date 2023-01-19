7252e0c6353b6d36998dc8d11cbbd2043b220682
Gaia Noir <tlaliketumile2@gmail.com>
2023-01-19 19:28:11 +0200

    Create check winner function

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 function getComputerChoice(){
     const choice = ["Rock", "Paper", "Scissors"];
     const random = Math.floor(Math.random() * choice.length);
-    console.log(choice[random])
+    return choice[random];
 }
+
+function checkWinner(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "Tie"
+    } else if(
+        (playerSelection == "rock" && computerSelection== "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    } else{
+        return "Computer";
+    }
+}
+ function gameRound() {
+    const results = checkWinner(playerSelection, computerSelection);
+    if(results == "Tie"){
+        return "It's a tie"
+    } else if(results == "Player") {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+const playerSelection = prompt("Rock, Paper, Scissors? ").toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+
