b26b8a9fbf4576524ce77ea95a9033ac7de0f329
Gaia Noir <tlaliketumile2@gmail.com>
2023-02-04 16:23:52 +0200

    Add top score function

--- app.js
+++ app.js
@@ -24,97 +24,109 @@ let tieScore = 0;
 let winningScore = 5;
 let isGameOver = false;
 
+
 rock.addEventListener('click', function(){
-    if(!isGameOver){
-        if(getComputerChoice() == "scissors"){
-            playerScore += 1;
-            playerDisplay.textContent = playerScore;
-        } else if(getComputerChoice() == "paper"){
-            computerScore += 1;
-            computerDisplay.textContent = computerScore;
-        } else {
-            tieScore += 1;
-            tieDisplay.textContent = tieScore;
-        };
-    }
-    if(playerScore === winningScore || computerScore === winningScore){
-        isGameOver = true;
-        if(isGameOver){
-            if(playerScore > computerScore){
-                h1.innerText = 'Congrats! You won! You beat the robot!';
-                body.appendChild(h1);
-            } else if(computerScore > playerScore){
-                h1.innerText = 'Uh oh! You lost! The robots have taken over!';
-                body.appendChild(h1);
+    if(parseInt(topScore.value) !== 0){
+        if(!isGameOver){
+            if(getComputerChoice() == "scissors"){
+                playerScore += 1;
+                playerDisplay.textContent = `Player: ${playerScore}`;
+            } else if(getComputerChoice() == "paper"){
+                computerScore += 1;
+                computerDisplay.textContent = `Computer: ${computerScore}`;
+            } else {
+                tieScore += 1;
+                tieDisplay.textContent = `Ties: ${tieScore}`;
+            };
+        }
+        if(playerScore === winningScore || computerScore === winningScore){
+            isGameOver = true;
+            if(isGameOver){
+                if(playerScore > computerScore){
+                    h1.innerText = 'Congrats! You won! You beat the robot!';
+                    body.appendChild(h1);
+                } else if(computerScore > playerScore){
+                    h1.innerText = 'Uh oh! You lost! The robots have taken over!';
+                    body.appendChild(h1);
+                };
             };
         };
-    };
-    
+    }
 });
 
 paper.addEventListener('click', function(){
-    if(!isGameOver){
-        if(getComputerChoice() == "rock"){
-            playerScore += 1;
-            playerDisplay.textContent = playerScore;
-        } else if(getComputerChoice() == "scissors"){
-            computerScore += 1;
-            computerDisplay.textContent = computerScore;
-        } else {
-            tieScore += 1;
-            tieDisplay.textContent = tieScore;
-        };
-    }
-    if(playerScore === winningScore || computerScore === winningScore){
-        isGameOver = true;
-        if(isGameOver == true){
-            if(playerScore > computerScore){
-                h1.innerText = 'Congrats! You won! You beat the robot!';
-                body.appendChild(h1);
-            } else if(computerScore > playerScore){
-                h1.innerText = 'Uh oh! You lost! The robots have taken over!';
-                body.appendChild(h1);
+    if(parseInt(topScore.value) !== 0){
+        if(!isGameOver){
+            if(getComputerChoice() == "rock"){
+                playerScore += 1;
+                playerDisplay.textContent =`Player: ${playerScore}`;
+            } else if(getComputerChoice() == "scissors"){
+                computerScore += 1;
+                computerDisplay.textContent = `Computer: ${computerScore}`;
+            } else {
+                tieScore += 1;
+                tieDisplay.textContent = `Ties: ${tieScore}`;
             };
-        };
-    };     
+        }
+        if(playerScore === winningScore || computerScore === winningScore){
+            isGameOver = true;
+            if(isGameOver){
+                if(playerScore > computerScore){
+                    h1.innerText = 'Congrats! You won! You beat the robot!';
+                    body.appendChild(h1);
+                } else if(computerScore > playerScore){
+                    h1.innerText = 'Uh oh! You lost! The robots have taken over!';
+                    body.appendChild(h1);
+                };
+            };
+        };  
+    }   
 });
 
 scissors.addEventListener('click', function(){
-    if(!isGameOver){
-        if(getComputerChoice() == "paper"){
-            playerScore += 1;
-            playerDisplay.textContent = playerScore;
-        } else if(getComputerChoice() == "rock"){
-            computerScore += 1;
-            computerDisplay.textContent = computerScore;
-        } else {
-            tieScore += 1;
-            tieDisplay.textContent = tieScore;
-        };
-    }   
-    if(playerScore === winningScore || computerScore === winningScore){
-        isGameOver = true;
-        if(isGameOver == true){
-            if(playerScore > computerScore){
-                h1.innerText = 'Congrats! You won! You beat the robot!';
-                body.appendChild(h1);
-            } else if(computerScore > playerScore){
-                h1.innerText = 'Uh oh! You lost! The robots have taken over!';
-                body.appendChild(h1);
+    if(parseInt(topScore.value) !== 0){
+        if(!isGameOver){
+            if(getComputerChoice() == "paper"){
+                playerScore += 1;
+                playerDisplay.textContent = `Player: ${playerScore}`;
+            } else if(getComputerChoice() == "rock"){
+                computerScore += 1;
+                computerDisplay.textContent = `Computer: ${computerScore}`;
+            } else {
+                tieScore += 1;
+                tieDisplay.textContent = `Ties: ${tieScore}`;
             };
-            
+        }   
+        if(playerScore === winningScore || computerScore === winningScore){
+            isGameOver = true;
+            if(isGameOver == true){
+                if(playerScore > computerScore){
+                    h1.innerText = 'Congrats! You won! You beat the robot!';
+                    body.appendChild(h1);
+                } else if(computerScore > playerScore){
+                    h1.innerText = 'Uh oh! You lost! The robots have taken over!';
+                    body.appendChild(h1);
+                };
+                
+            };
         };
-    };
+    }
 });
 
-topScore.querySelector('change', functio)
+topScore.addEventListener('change', function(){
+    winningScore = parseInt(this.value);
+    reset();
+});
 
-resetButton.addEventListener('click', function(){
-    let isGameOver = false;
-    let playerScore = 0;
-    let computerScore = 0;
-    let tieScore = 0;
-    playerDisplay.textContent = playerScore;
-    computerDisplay.textContent = computerScore;
-    tieDisplay.textContent = tieScore;
-})
+resetButton.addEventListener('click', reset);
+
+function reset(){
+    isGameOver = false;
+    playerScore = 0;
+    computerScore = 0;
+    tieScore = 0;
+    playerDisplay.textContent = `Player: ${playerScore}`;
+    computerDisplay.textContent = `Computer: ${computerScore}`;
+    tieDisplay.textContent = `Ties: ${tieScore}`;
+    h1.remove();
+}
